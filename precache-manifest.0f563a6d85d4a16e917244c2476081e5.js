e00f91dde309f7c16fd122ea132e6803632abd3c
Michael Vytlingam <michael.vytlingam@gmail.com>
2019-10-26 14:48:09 -0400

    Updates

---
+++ precache-manifest.0f563a6d85d4a16e917244c2476081e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c941301d70e53556b0b306253160fa4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0190f167df649954ef2f",
+    "url": "/portfolio/static/css/main.cd345123.chunk.css"
+  },
+  {
+    "revision": "e40b20e9c49549972a19",
+    "url": "/portfolio/static/js/2.86818ab3.chunk.js"
+  },
+  {
+    "revision": "0190f167df649954ef2f",
+    "url": "/portfolio/static/js/main.94cab0c5.chunk.js"
+  },
+  {
+    "revision": "ec71d439d0c7dc20b2e0",
+    "url": "/portfolio/static/js/runtime-main.f347cb88.js"
+  },
+  {
+    "revision": "2f67d006b09e74286700c5ec932f6f54",
+    "url": "/portfolio/static/media/logo.2f67d006.jpg"
+  }
+]);
