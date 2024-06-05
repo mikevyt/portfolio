d65eccd53a0c4cd84d5de3d8fe814511546f9bb0
Michael Vytlingam <michael.vytlingam@gmail.com>
2024-06-04 21:20:28 -0700

    update resume

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import { SocialIcons } from '../../components/SocialIcons';
-import { ThemeContext } from '../App';
+import React from "react";
+import { Link } from "react-scroll";
+import { SocialIcons } from "../../components/SocialIcons";
+import { ThemeContext } from "../App";
 
-import './Footer.scss';
+import "./Footer.scss";
 
 export const Footer = () => {
-    const isDarkTheme = React.useContext(ThemeContext);
-    return (
-        <footer className={`footer ${isDarkTheme && 'dark-layer-1'}`}>
-            <section className="container columns">
-                <div className="column">
-                    <SocialIcons />
-                    <p>© 2022 Michael Vytlingam. All Rights Reserved.</p>
-                </div>
-                <div className="column footer-right">
-                    <Link to="splash" smooth>
-                        <i className="fas fa-arrow-up"></i>
-                    </Link>
-                </div>
-            </section>
-        </footer>
-    );
+  const isDarkTheme = React.useContext(ThemeContext);
+  return (
+    <footer className={`footer ${isDarkTheme && "dark-layer-1"}`}>
+      <section className="container columns">
+        <div className="column">
+          <SocialIcons />
+          <p>© 2024 Michael Vytlingam. All Rights Reserved.</p>
+        </div>
+        <div className="column footer-right">
+          <Link to="splash" smooth>
+            <i className="fas fa-arrow-up"></i>
+          </Link>
+        </div>
+      </section>
+    </footer>
+  );
 };
--- src/App/Splash/Splash.tsx
+++ src/App/Splash/Splash.tsx
@@ -1,56 +1,74 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import './Splash.scss';
-import ResumePath from '../../assets/resume.pdf';
+import React from "react";
+import { Link } from "react-scroll";
+import ResumePath from "../../assets/resume.pdf";
+import "./Splash.scss";
 
-
-import withAnimation from '../../HOCs/withAnimation/withAnimation';
-import { SocialIcons } from '../../components/SocialIcons';
-import { ThemeContext } from '../App';
+import withAnimation from "../../HOCs/withAnimation/withAnimation";
+import { SocialIcons } from "../../components/SocialIcons";
+import { ThemeContext } from "../App";
 
 const Splash = () => (
-    <section className="container splash">
-        <div className="splash-content">
-            <SplashText />
-            <div className="portrait" />
-        </div>
-        <Link
-            className="arrow-down animated bounce delay-2s infinite slow"
-            to="about-me"
-            smooth
-        >
-            <i className="fas fa-chevron-down"></i>
-        </Link>
-    </section>
+  <section className="container splash">
+    <div className="splash-content">
+      <SplashText />
+      <div className="portrait" />
+    </div>
+    <Link
+      className="arrow-down animated bounce delay-2s infinite slow"
+      to="about-me"
+      smooth
+    >
+      <i className="fas fa-chevron-down"></i>
+    </Link>
+  </section>
 );
 
 const SplashText = () => {
-    const isDarkTheme = React.useContext(ThemeContext);
-    return <div>
-        <h1 className={`is-1 title ${isDarkTheme && 'dark-title'}`}>
-            Hey, I'm Michael.{' '}
-            <span role="img" aria-label="wave">
-                👋🏽
-            </span>
-        </h1>
-        <h2 className={`subtitle ${isDarkTheme && 'dark-subtitle'}`}>
-            I like to build beautiful and smart things working with wonderful people.
-            <br/>
-            I’m a Software Engineer at <a className={`${isDarkTheme ? 'dark-underline' : 'underline'}`} href="https://www.snowflake.com/">Snowflake</a> working on the <a className={`${isDarkTheme ? 'dark-underline' : 'underline'}`} href="https://www.snowflake.com/en/data-cloud/marketplace/">Marketplace</a>.
-        </h2>
-        <div className="button-row">
-            <a
-                href={ResumePath}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`button is-info ${isDarkTheme ? 'is-light' : ''}`}
-            >
-                Check out my resume
-            </a>
-            <div className="vertical-line" />
-            <SocialIcons />
-        </div>
+  const isDarkTheme = React.useContext(ThemeContext);
+  return (
+    <div>
+      <h1 className={`is-1 title ${isDarkTheme && "dark-title"}`}>
+        Hey, I'm Michael.{" "}
+        <span role="img" aria-label="wave">
+          👋🏽
+        </span>
+      </h1>
+      <h2 className={`subtitle ${isDarkTheme && "dark-subtitle"}`}>
+        I like to build beautiful and smart things working with wonderful
+        people.
+        <br />
+        I’m a Software Engineer at{" "}
+        <a
+          className={`${isDarkTheme ? "dark-underline" : "underline"}`}
+          href="https://www.snowflake.com/"
+        >
+          Snowflake
+        </a>{" "}
+        working on the{" "}
+        <a
+          className={`${isDarkTheme ? "dark-underline" : "underline"}`}
+          href="https://www.snowflake.com/en/data-cloud/marketplace/"
+        >
+          Marketplace
+        </a>
+        .
+      </h2>
+      <div className="button-row">
+        <a
+          href={ResumePath}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`button is-info has-text-weight-medium ${
+            isDarkTheme ? "is-light" : ""
+          }`}
+        >
+          Check out my resume
+        </a>
+        <div className="vertical-line" />
+        <SocialIcons />
+      </div>
     </div>
+  );
 };
 
 export default withAnimation(Splash);
