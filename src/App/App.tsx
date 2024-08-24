14c1276f5c871af9126d5709e9eab6211d851edf
Michael Vytlingam <michael.vytlingam@gmail.com>
2024-08-24 13:08:24 -0700

    fix bugs

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,78 +1,72 @@
-import React from 'react';
-import { Element } from 'react-scroll';
+import React from "react";
+import { Element } from "react-scroll";
 
-import './App.scss';
+import "./App.scss";
 
-import Splash from './Splash/Splash';
-import Knowledge from './Knowledge/Knowledge';
-import ContactMe from './ContactMe/ContactMe';
-import { NavBar } from './NavBar/NavBar';
-import { AboutMe } from './AboutMe/AboutMe';
-import { Experiences } from './Experiences/Experiences';
-import { Footer } from './Footer/Footer';
+import { AboutMe } from "./AboutMe/AboutMe";
+import ContactMe from "./ContactMe/ContactMe";
+import { Experiences } from "./Experiences/Experiences";
+import { Footer } from "./Footer/Footer";
+import Knowledge from "./Knowledge/Knowledge";
+import { NavBar } from "./NavBar/NavBar";
+import Splash from "./Splash/Splash";
 
 export const ThemeContext = React.createContext(false);
 export const ScrolledContext = React.createContext(false);
-const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const prefersDarkScheme = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
 
 export const App = () => {
-    const [hasScrolled, setHasScrolled] = React.useState(false);
-    const [isDarkTheme, setIsDarkTheme] = React.useState(prefersDarkScheme);
+  const [hasScrolled, setHasScrolled] = React.useState(false);
+  const [isDarkTheme, setIsDarkTheme] = React.useState(prefersDarkScheme);
 
-    // Set overscroll color
-    document.getElementsByTagName('html')[0].style.backgroundColor = isDarkTheme ? '#242424' : '#FFF';
+  // Set overscroll color
+  document.getElementsByTagName("html")[0].style.backgroundColor = isDarkTheme
+    ? "#242424"
+    : "#FFF";
 
-    const handleScroll = React.useCallback(() => {
-        if (window.scrollY > 0 && !hasScrolled) {
-            setHasScrolled(true);
-        } else if (window.scrollY === 0 && hasScrolled) {
-            setHasScrolled(false);
-        }
-    }, [hasScrolled]);
+  const handleScroll = React.useCallback(() => {
+    if (window.scrollY > 0 && !hasScrolled) {
+      setHasScrolled(true);
+    } else if (window.scrollY === 0 && hasScrolled) {
+      setHasScrolled(false);
+    }
+  }, [hasScrolled]);
 
-    React.useEffect(() => {
-        document.addEventListener('scroll', handleScroll);
-        return () => document.removeEventListener('scroll', handleScroll);
-    }, [handleScroll]);
+  React.useEffect(() => {
+    document.addEventListener("scroll", handleScroll);
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
-    return (
-        <ThemeContext.Provider value={isDarkTheme}>
-            <ScrolledContext.Provider value={hasScrolled}>
-                <div className={`${isDarkTheme && 'dark-background'}`}>
-                    <NavBar onThemeChange={(isDarkTheme) => setIsDarkTheme(isDarkTheme)} />
-                    <Element id="splash" name="splash">
-                        <Splash />
-                    </Element>
-                    <Element id="about-me" name="about-me" className="body-section">
-                        <AboutMe />
-                    </Element>
-                    <Element
-                        id="knowledge"
-                        name="knowledge"
-                        className="body-section"
-                    >
-                        <Knowledge />
-                    </Element>
-                    <Element
-                        id="experience"
-                        name="experience"
-                        className="body-section"
-                    >
-                        <Experiences />
-                    </Element>
-                    {/* <Element id="projects" name="projects" className="body-section">
+  return (
+    <ThemeContext.Provider value={isDarkTheme}>
+      <ScrolledContext.Provider value={hasScrolled}>
+        <div {...(isDarkTheme ? { className: "dark-background" } : {})}>
+          <NavBar
+            onThemeChange={(isDarkTheme) => setIsDarkTheme(isDarkTheme)}
+          />
+          <Element id="splash" name="splash">
+            <Splash />
+          </Element>
+          <Element id="about-me" name="about-me" className="body-section">
+            <AboutMe />
+          </Element>
+          <Element id="knowledge" name="knowledge" className="body-section">
+            <Knowledge />
+          </Element>
+          <Element id="experience" name="experience" className="body-section">
+            <Experiences />
+          </Element>
+          {/* <Element id="projects" name="projects" className="body-section">
                         <Projects />
                     </Element> */}
-                    <Element
-                        id="contact-me"
-                        name="contact-me"
-                        className="body-section"
-                    >
-                        <ContactMe />
-                    </Element>
-                    <Footer />
-                </div>
-            </ScrolledContext.Provider>
-        </ThemeContext.Provider>
-    );
-}
+          <Element id="contact-me" name="contact-me" className="body-section">
+            <ContactMe />
+          </Element>
+          <Footer />
+        </div>
+      </ScrolledContext.Provider>
+    </ThemeContext.Provider>
+  );
+};
--- src/App/Experiences/Experiences.tsx
+++ src/App/Experiences/Experiences.tsx
@@ -1,51 +1,51 @@
-import React from 'react';
-import { ThemeContext } from '../App';
-import './Experiences.scss';
+import React from "react";
+import { ThemeContext } from "../App";
+import "./Experiences.scss";
 
-import JobCard from './JobCard/JobCard';
+import JobCard from "./JobCard/JobCard";
 
 export const Experiences = () => {
-    const isDarkTheme = React.useContext(ThemeContext);
-    const [update, causeUpdate] = React.useState(false);
+  const isDarkTheme = React.useContext(ThemeContext);
+  const [update, causeUpdate] = React.useState(false);
 
-    const handleResize = () => causeUpdate(!update);
+  const handleResize = () => causeUpdate(!update);
 
-    React.useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    });
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
 
-    const jobs = [
-        {
-            logo: 'snowflake.png',
-            company: 'Snowflake',
-            position: 'Software Engineer',
-            date: 'September 2022 - Present',
-            location: 'San Mateo, CA',
-            body: `
+  const jobs = [
+    {
+      logo: "snowflake.png",
+      company: "Snowflake",
+      position: "Software Engineer",
+      date: "September 2022 - Present",
+      location: "San Mateo, CA",
+      body: `
                 <b>Tech:</b> TypeScript, React, Express, JEST
             `,
-        },
-        {
-            logo: 'snowflake.png',
-            company: 'Snowflake',
-            position: 'Software Engineering Intern',
-            date: 'May 2021 - August 2021',
-            location: 'San Mateo, CA (Remote)',
-            body: `
+    },
+    {
+      logo: "snowflake.png",
+      company: "Snowflake",
+      position: "Software Engineering Intern",
+      date: "May 2021 - August 2021",
+      location: "San Mateo, CA (Remote)",
+      body: `
                 Built out captivating customer experiences on the Snowflake Data Marketplace product.
                 <br/>
                 <br/>
                 <b>Tech:</b> TypeScript, React, Express, JEST
             `,
-        },
-        {
-            logo: 'bb.png',
-            company: 'BlackBerry',
-            position: 'Machine Learning Engineering Student',
-            date: 'September 2020 - December 2020',
-            location: 'Ottawa, ON (Remote)',
-            body: `
+    },
+    {
+      logo: "bb.png",
+      company: "BlackBerry",
+      position: "Machine Learning Engineering Student",
+      date: "September 2020 - December 2020",
+      location: "Ottawa, ON (Remote)",
+      body: `
                 I improved my team's existing NLP-based open source license 
                 detection pipeline, used to automate open source compliance.
                 I reduced downloading and file crawling time by 70% after introducing multithreading and
@@ -54,14 +54,14 @@ export const Experiences = () => {
                 <br/>
                 <b>Tech:</b> Python, Pytorch, Jenkins
             `,
-        },
-        {
-            logo: 'cryptonumerics.jpg',
-            company: 'CryptoNumerics',
-            position: 'Full Stack Engineering Intern',
-            date: 'January 2020 - April 2020',
-            location: 'Toronto, ON',
-            body: `
+    },
+    {
+      logo: "cryptonumerics.jpg",
+      company: "CryptoNumerics",
+      position: "Full Stack Engineering Intern",
+      date: "January 2020 - April 2020",
+      location: "Toronto, ON",
+      body: `
                 I built and shipped new features to our CN-Protect
                 application; a tool for dataset de-identification. I also built a 
                 new reporting tool to be used across all of our products in order to keep the
@@ -70,14 +70,14 @@ export const Experiences = () => {
                 <br/>
                 <b>Tech:</b> Java, Electron, React, Redux, JEST
             `,
-        },
-        {
-            logo: 'uw.jpg',
-            company: 'University of Waterloo',
-            position: 'Machine Learning Researcher',
-            date: 'September 2019 - December 2019',
-            location: 'Waterloo, ON',
-            body: `
+    },
+    {
+      logo: "uw.jpg",
+      company: "University of Waterloo",
+      position: "Machine Learning Researcher",
+      date: "September 2019 - December 2019",
+      location: "Waterloo, ON",
+      body: `
                 I took part in training real-time object detection model of 
                 kitchen items to help a robot operate in its surroundings. 
                 I can't tell you the specifics as we're still working on it, 
@@ -86,15 +86,15 @@ export const Experiences = () => {
                 <br/>
                 <b>Tech:</b> Darknet, YOLO, TensorFlow
             `,
-        },
-        {
-            logo: 'gd.jpg',
-            coverPhoto: 'gd-cover.jpg',
-            company: 'Grain Discovery',
-            position: 'Software Engineering Intern',
-            date: 'May 2019 - August 2019',
-            location: 'Toronto, ON',
-            body: `
+    },
+    {
+      logo: "gd.jpg",
+      coverPhoto: "gd-cover.jpg",
+      company: "Grain Discovery",
+      position: "Software Engineering Intern",
+      date: "May 2019 - August 2019",
+      location: "Toronto, ON",
+      body: `
                 I worked on Grain Discovery's agricultural trading marketplace. 
                 Introducing effective UI/UX was a challenge due to the wide demographics
                 of users using the platform, from farmers to business traders. 
@@ -110,15 +110,15 @@ export const Experiences = () => {
                 Hyperledger Fabric (TypeScript), Bash, Docker, Semantic UI, 
                 Lottie, Figma, JEST, Enzyme
             `,
-        },
-        {
-            logo: 'engineering.jpg',
-            coverPhoto: 'engineering-cover.jpg',
-            company: 'ENGINEERING.com',
-            position: 'Software Engineering Intern',
-            date: 'September 2018 - December 2018',
-            location: 'Toronto, ON',
-            body: `
+    },
+    {
+      logo: "engineering.jpg",
+      coverPhoto: "engineering-cover.jpg",
+      company: "ENGINEERING.com",
+      position: "Software Engineering Intern",
+      date: "September 2018 - December 2018",
+      location: "Toronto, ON",
+      body: `
                 I was working on ENGINEERING.com's online collaboration platform, ProjectBoard. 
                 I designed and developed the new home page for the platform as well as the user 
                 profile pages. I also improved the analytics dashboard, incorporating new data 
@@ -127,14 +127,14 @@ export const Experiences = () => {
                 <br/>
                 <b>Tech:</b> Vue, Vuex, Sass, Fabric.js, Go, MySQL, TestCafe
             `,
-        },
-        {
-            logo: 'snyder.jpg',
-            company: 'Snyder Construction',
-            position: 'Assistant Project Manager',
-            date: 'January 2018 - April 2018',
-            location: 'Whitby, ON',
-            body: `
+    },
+    {
+      logo: "snyder.jpg",
+      company: "Snyder Construction",
+      position: "Assistant Project Manager",
+      date: "January 2018 - April 2018",
+      location: "Whitby, ON",
+      body: `
                 I was involved in all aspects of Snyder's construction
                 projects, from price estimation to closeout. I drew up 
                 project schedules, defined scopes and ensured they were 
@@ -144,56 +144,61 @@ export const Experiences = () => {
                 <b>Taught Me:</b> the importance of getting a job done quickly 
                 and effectively, how to drive massive pickup trucks. 🚚
             `,
-        },
-        {
-            logo: 'mvgd.jpg',
-            coverPhoto: 'kendo.jpg',
-            company: 'MVGD',
-            position: 'Freelance Graphic Designer',
-            date: 'Dec 2015 - Sept 2018',
-            location: 'Remote',
-            body: `
+    },
+    {
+      logo: "mvgd.jpg",
+      coverPhoto: "kendo.jpg",
+      company: "MVGD",
+      position: "Freelance Graphic Designer",
+      date: "Dec 2015 - Sept 2018",
+      location: "Remote",
+      body: `
                 I created logos, banners, documents, and websites for 
                 various clients, including the University of Toronto 
                 Kendo Club, LSPDFR.com, and Rubiar.
             `,
-        },
-    ];
+    },
+  ];
 
-    const isMobile = window.innerWidth < 1024;
+  const isMobile = window.innerWidth < 1024;
 
-    const columns: any[][] = [[], []];
-    if (isMobile) {
-        columns[0] = jobs;
-    } else {
-        for (let i = 0; i < jobs.length - 1; i += 2) {
-            columns[0].push(jobs[i]);
-            columns[1].push(jobs[i + 1]);
-        }
+  const columns: any[][] = [[], []];
+  if (isMobile) {
+    columns[0] = jobs;
+  } else {
+    for (let i = 0; i < jobs.length - 1; i += 2) {
+      columns[0].push(jobs[i]);
+      columns[1].push(jobs[i + 1]);
     }
+  }
 
-    console.log(columns);
+  return (
+    <div className="container">
+      <h1
+        className={`title has-text-centered is-3 ${
+          isDarkTheme && "dark-title"
+        }`}
+      >
+        Experiences
+      </h1>
 
-    return (
-        <div className="container">
-            <h1 className={`title has-text-centered is-3 ${isDarkTheme && 'dark-title'}`}>Experiences</h1>
-
-            {isMobile ? columns[0].map((job, index) => (
-                <div className="experiences" key={index}>
-                    <JobCard job={job} />
-                </div>
-            )) : (
-                <div className="two-column">
-                    {columns.map((column, cIndex) =>
-                        <div className="experiences" key={cIndex} >
-                            {column.map((job, index) => (
-                                <JobCard key={index} job={job} />
-                            ))}
-                        </div>
-                    )}
-                </div>
-            )}
+      {isMobile ? (
+        columns[0].map((job, index) => (
+          <div className="experiences" key={index}>
+            <JobCard job={job} />
+          </div>
+        ))
+      ) : (
+        <div className="two-column">
+          {columns.map((column, cIndex) => (
+            <div className="experiences" key={cIndex}>
+              {column.map((job, index) => (
+                <JobCard key={index} job={job} />
+              ))}
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
-
